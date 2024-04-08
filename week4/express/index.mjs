ba7fef734c8e2a5fef327bb5d91c51ce4823ff26
Emad4770 <emad47.n7@gmail.com>
2024-04-08 20:51:03 +0200

    routed /about with copilot

--- week4/express/index.mjs
+++ week4/express/index.mjs
@@ -9,6 +9,12 @@ app.get('/', (req, res) => {
     res.send('Hello There!')
 })
 
+app.get('/about', (req, res) => {
+    res.send('About Us')
+}
+)
+
+
 
 app.listen(3000, () => {
     console.log("Application is running!!");
